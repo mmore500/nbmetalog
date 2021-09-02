0e8496c9eb04f296dcb385803c0afce4dbcbc2ce
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-01 23:42:42 -0400

    Add note about alternate impl

--- nbmetalog/get_dataframe_full_digest.py
+++ nbmetalog/get_dataframe_full_digest.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 from . import _except_return_none
 
+# also considered https://newbedev.com/how-to-generate-a-hash-or-checksum-value-on-python-dataframe-created-from-a-fixed-width-file
+# which is slower
+
 @_except_return_none
 def get_dataframe_full_digest(df):
     # adapted from https://stackoverflow.com/a/47800021
