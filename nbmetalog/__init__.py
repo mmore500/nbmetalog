739ae5d53e56538216b587a8da598514da4ff32d
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-10-06 15:17:48 -0400

    Bump version: 0.2.4 → 0.2.5

--- nbmetalog/__init__.py
+++ nbmetalog/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = 'm.more500@gmail.com'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 from ._except_return_none import _except_return_none
 from .get_dataframe_full_digest import get_dataframe_full_digest
